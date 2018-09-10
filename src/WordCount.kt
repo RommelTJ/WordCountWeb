525b22d5b948013c6f61286a9dfb3135fe3d09c0
Rommel Rico <me@rommelrico.com>
2018-09-10 08:49:19 -0700

    Getting access to text via input tag

--- src/WordCount.kt
+++ src/WordCount.kt
@@ -1,9 +1,11 @@
+import org.w3c.dom.HTMLInputElement
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
     println("Hello World")
     val myButton = document.getElementById("myButton")!!
     myButton.addEventListener("click", {
-        println("Clicked")
+        val inputValue = (document.getElementById("myInput")!! as HTMLInputElement).value
+        println(inputValue)
     })
 }
