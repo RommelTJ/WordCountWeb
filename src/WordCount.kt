50babdd0e3a347b83eda595275f5bc9194d92e9b
Rommel Rico <me@rommelrico.com>
2018-09-10 08:54:18 -0700

    Copying code from older project straight into this project

--- src/WordCount.kt
+++ src/WordCount.kt
@@ -2,10 +2,40 @@ import org.w3c.dom.HTMLInputElement
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
-    println("Hello World")
     val myButton = document.getElementById("myButton")!!
     myButton.addEventListener("click", {
-        val inputValue = (document.getElementById("myInput")!! as HTMLInputElement).value
-        println(inputValue)
+        val allTheWords = (document.getElementById("myInput")!! as HTMLInputElement).value
+
+        // Make a list of all the words separated out.
+        val words = allTheWords
+                .toLowerCase()
+                .replace(",", "")
+                .replace(".", "")
+                .replace("!", "")
+                .replace("?", "")
+                .replace("\n", " ")
+                .split(" ")
+
+        // Get a counted list of all the words.
+        val wordMap = mutableMapOf<String, Int>()
+        for (word in words) {
+            if (wordMap.containsKey(word)) {
+                val currentCount = wordMap[word]!!
+                wordMap[word] = currentCount + 1
+            } else if (word != "") {
+                wordMap[word] = 1
+            }
+        }
+
+        // Convert map to list
+        val wordList = wordMap.toList()
+
+        // Sort the list.
+        val sortedList = wordList.sortedWith(compareByDescending({it.second}))
+
+        // Print a sorted list of most popular words.
+        for (word in sortedList) {
+            println("${word.first} - ${word.second}")
+        }
     })
 }
