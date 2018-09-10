e4795f7a8ed98c2655cffd8eae2dee8741d93a00
Rommel Rico <me@rommelrico.com>
2018-09-10 08:46:18 -0700

    Getting access to the button

--- src/WordCount.kt
+++ src/WordCount.kt
@@ -1,3 +1,9 @@
+import kotlin.browser.document
+
 fun main(args: Array<String>) {
     println("Hello World")
+    val myButton = document.getElementById("myButton")!!
+    myButton.addEventListener("click", {
+        println("Clicked")
+    })
 }
